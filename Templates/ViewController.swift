99c983ce4daf57a239f97d6e0d57a5a16506d1f2
Simon Nickel <simonnickel@googlemail.com>
2020-06-22 15:00:37 +0200

    Initial Commit

---
+++ Templates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Templates
+//
+//  Created by simonnickel on 22.06.20.
+//  Copyright © 2020 simonnickel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
