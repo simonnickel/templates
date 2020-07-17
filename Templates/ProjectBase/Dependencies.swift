24696cc7757e954f291d8d301c688ce02b9f7eed
Simon Nickel <simonnickel@googlemail.com>
2020-07-17 13:21:05 +0200

    Add dependency init to app and scene delegate

---
+++ Templates/ProjectBase/Dependencies.swift
@@ -0,0 +1,13 @@
+//
+//  Dependencies.swift
+//  Templates
+//
+//  Created by Simon Nickel on 17.07.20.
+//  Copyright © 2020 simonnickel. All rights reserved.
+//
+
+import Foundation
+
+class Dependencies {
+    
+}
